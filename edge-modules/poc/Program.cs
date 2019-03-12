1249e6214962baa5ed644de04f299a6e037f84d2
Anca Antochi <ancan@microsoft.com>
2019-03-12 13:17:25 -0700

    New version

--- edge-modules/poc/Program.cs
+++ edge-modules/poc/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+using System.Text;
 using System.Threading.Tasks;
 using DotNetty.Codecs.Mqtt.Packets;
 using Makaretu.Dns;
@@ -12,6 +14,7 @@ using Microsoft.Azure.Devices.Client.Transport.Mqtt;
 using Microsoft.Azure.Devices.Provisioning.Client;
 using Microsoft.Azure.Devices.Provisioning.Client.Transport;
 using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
 using Zeroconf;
 using Message = Microsoft.Azure.Devices.Client.Message;
 using ServiceProfile = Microsoft.Azure.Devices.Client.ServiceProfile;
@@ -22,6 +25,7 @@ namespace poc
     {
         static void Main(string[] args)
         {
+            var tcs = new TaskCompletionSource<bool>();
             string advertise = Environment.GetEnvironmentVariable("Advertise");
 
             if (Boolean.TryParse(advertise, out bool adv))
@@ -31,17 +35,15 @@ namespace poc
             }
             else
             {
-                Console.WriteLine("Discover");
-                Discover();
+                //Console.WriteLine("Discover");
+                Discover(tcs.Task);
             }
-
-
-            var tcs = new TaskCompletionSource<bool>();
+            
             tcs.Task.Wait();
             Console.ReadLine();
         }
 
-        private static void Discover()
+        private static void Discover(Task<bool> task)
         {
 
             //var r = ZeroconfResolver.BrowseDomainsAsync().Result;
@@ -50,57 +52,117 @@ namespace poc
             //    Console.WriteLine($"{r1.Key}");
             //}
 
-            //IList<IPEndPoint> ips = new List<IPEndPoint>();
-            //string serviceName = "_dpsproxy._tcp.local.";
-            //var zeroconfHosts = ZeroconfResolver.ResolveAsync(serviceName).Result;
-            //foreach (var zeroConfHost in zeroconfHosts)
-            //{
-            //    Console.WriteLine($"DisplayName {zeroConfHost.DisplayName}");
-            //    if (zeroConfHost.DisplayName.Equals("dpsProxy", StringComparison.CurrentCultureIgnoreCase))
-            //    {
-            //        foreach (var ipAddress in zeroConfHost.IPAddresses)
-            //        {
-            //            Console.WriteLine($"IP {ipAddress} PORT {zeroConfHost.Services[serviceName].Port}");
-            //            ips.Add((new IPEndPoint(IPAddress.Parse(ipAddress), zeroConfHost.Services[serviceName].Port)));
-            //        }
+            IList<IPEndPoint> ips = new List<IPEndPoint>();
+            
+            bool.TryParse(Environment.GetEnvironmentVariable("USE_DPS_PROXY_DISCOVERY"), out bool useDpsProxy);
+            if (useDpsProxy)
+            {
+                string dpsServiceName = "_dpsproxy._tcp.local.";
+                Console.WriteLine($"Discovering Local DPS Proxy by service name {dpsServiceName}... \n");
+                
+                var zeroconfHosts = ZeroconfResolver.ResolveAsync(dpsServiceName).Result;
+                foreach (var zeroConfHost in zeroconfHosts)
+                {
+                    if (zeroConfHost.DisplayName.Equals("dpsProxy", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        foreach (var ipAddress in zeroConfHost.IPAddresses)
+                        {
+                            if (ipAddress.StartsWith("10"))
+                            {
+                                Console.WriteLine(
+                                    $"Discovered Local DPS Proxy \n  IP {ipAddress} \n  PORT {zeroConfHost.Services[dpsServiceName].Port} \n");
+                                ips.Add(
+                                    (new IPEndPoint(IPAddress.Parse(ipAddress),
+                                        zeroConfHost.Services[dpsServiceName].Port)));
+                            }
+                        }
 
-            //    }
-            //}
+                    }
+                }
+            }
+            else
+            {
+                string proxyIp = Environment.GetEnvironmentVariable("PROXY_IP");
+                if (!string.IsNullOrEmpty(proxyIp))
+                {
+                    int proxyPort = int.Parse(Environment.GetEnvironmentVariable("PROXY_PORT"));
+                    ips.Add(new IPEndPoint(IPAddress.Parse(proxyIp), proxyPort));
+                    Console.WriteLine($"Proxy ip {proxyIp} \n");
+                }
+                else
+                {
+                    Console.WriteLine($"Provisioning without Local DPS Proxy \n");
+                }
+            }
 
-            //Console.WriteLine($"Discovered {ips.Count} services");
-
-            //if (ips.Count > 0)
-            //{
-            //string secondaryKey = Environment.GetEnvironmentVariable("SECONDARY_KEY");
-            //string globalDeviceEndpoint = "global.azure-devices-provisioning.net";
-            //string idScope = Environment.GetEnvironmentVariable("ID_SCOPE");
-            //string registrationId = Environment.GetEnvironmentVariable("REGISTARTION_ID");
-            //string primaryKey = Environment.GetEnvironmentVariable("PRIMARY_KEY");
-            //var transport = new ProvisioningTransportHandlerHttp();
-            //transport.Proxy = new WebProxy(ips[0].Address.ToString(), ips[0].Port);
-            //var dpsClient = ProvisioningDeviceClient.Create(globalDeviceEndpoint, idScope,
-            //    new SecurityProviderSymmetricKey(registrationId, primaryKey, secondaryKey), transport);
-            //var result = dpsClient.RegisterAsync().Result;
-
-            //Console.WriteLine(result);
-
-            string iothub = Environment.GetEnvironmentVariable("IOTHUB"); //result.AssignedHub
-            string deviceId = Environment.GetEnvironmentVariable("DEVICE_ID"); //result.DeviceId
+            string secondaryKey = Environment.GetEnvironmentVariable("SECONDARY_KEY");
+            string globalDeviceEndpoint = "edgehackathon3.pp0.azure-devices-provisioning-int.net";//"global.azure-devices-provisioning.net";
+            string idScope = "1989705015";//Environment.GetEnvironmentVariable("ID_SCOPE");
+            string registrationId = Environment.GetEnvironmentVariable("REGISTARTION_ID");
             string primaryKey = Environment.GetEnvironmentVariable("PRIMARY_KEY");
+            var transport = new ProvisioningTransportHandlerHttp();
+            if (ips.Count > 0)
+            {
+                transport.Proxy = new WebProxy($"http://{ips[0].Address}:{ips[0].Port}");
+            }
+
+            Console.WriteLine($"Provisioning using DPS Configuration: \n  Global endpoint: {globalDeviceEndpoint} \n  ID Scope: {idScope} \n  Registration Id: {registrationId}");
+            Console.WriteLine();
+
+            var dpsClient = ProvisioningDeviceClient.Create(globalDeviceEndpoint, idScope,
+                    new SecurityProviderSymmetricKey(registrationId, primaryKey, secondaryKey), transport);
+            
+            var result = dpsClient.RegisterAsync().Result;
+
+            Console.WriteLine("Successfully provisioned by DPS. \n");
+            string serviceName = "Winterfell";
+            Console.WriteLine($"Received DPS registration info \n  DeviceId: {result.DeviceId}, \n  IoT Hub: {result.AssignedHub}, \n  Local Edge Gateway service name: {serviceName}  <=== ***This is new configuration metadata from DPS*** \n");
+
+            string iothub = result.AssignedHub; //Environment.GetEnvironmentVariable("IOTHUB"); //result.AssignedHub
+            string deviceId = result.DeviceId; //Environment.GetEnvironmentVariable("DEVICE_ID"); //result.DeviceId
             var serviceDiscovery = new MDnsServiceDiscovery("edgehub");
+            //var t = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
+            //t.Proxy = new WebProxy($"http://{ips[0].Address}:{ips[0].Port}");
             var dc = DeviceClient.Create(serviceDiscovery, iothub, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, primaryKey),
                 new ITransportSettings[] { new MqttTransportSettings(TransportType.Mqtt_Tcp_Only) });
 
-            string edgeHub = serviceDiscovery.GetServiceProfileAsync().Result.Hostname;
-            Console.WriteLine($"Calling DeviceClient Open Iothub {iothub} deviceId {deviceId} edgeHub {edgeHub}");
+            //var m =ModuleClient.CreateFromConnectionString("GatewayHostname=edge.iot.microsoft.com;HostName=ancan-edge.azure-devices.net;DeviceId=ancan-device6;ModuleId=tempFilterFunctions;SharedAccessKey=/Dt060kAFfC0+5Iq9HRxLuHxJ66v3hN2D2aNJyxnZOM=", new ITransportSettings[] { t });
+            //m.OpenAsync().Wait();
 
+            Console.WriteLine($"Discovering Local Edge Gateway by service name {serviceName}... \n");
+            var edgeHub = serviceDiscovery.GetServiceProfileAsync().Result;
+            Console.WriteLine($"Discovered Local Edge Gateway \n  Hostname: {edgeHub.Hostname} \n  IP: {edgeHub.Addresses[0].Address} \n");
+            Console.WriteLine($"Connecting to {edgeHub.Hostname}");
             dc.OpenAsync().Wait();
-            Console.WriteLine("Connected");
-
-            dc.SendEventAsync(new Message()).Wait();
-            Console.WriteLine("Message sent");
-
-            //}
+            Console.WriteLine($"Connected to {edgeHub.Hostname}");
+            Random rnd = new Random();
+            int i = 0;
+            string[] funnyMessages = new string[]
+            {
+                "Hi Azure IoT! I am Jon Snow Targaryen-Stark, I know it's complicated, but you can call me the King in the North.",
+                "The irony that Whitewalkers use AWS Greengrass for their Edge needs is not lost on me.",
+                "I want to start a POC on connected dragons. Where do I get started ? There are so many docs!",
+                "People say I have a brooding look about me. Well that is just my resting face, deal with it.",
+                "Dragons are waaaay easier than dealing with certificates."
+                //"I probably, just may have, a slight chance of daddy issues.Maybe not, I am Jon Snow - The King in the North.",
+                
+            };
+            while (!task.IsCanceled)
+            {
+                var msg = new MessageBody()
+                {
+                    Message = funnyMessages[i % funnyMessages.Length] + " : Temperature in Winterfell",
+                    Temperature = rnd.NextDouble(),
+                    TimeCreated = DateTime.UtcNow
+                };
+                string dataBuffer = JsonConvert.SerializeObject(msg);
+                var eventMessage = new Message(Encoding.UTF8.GetBytes(dataBuffer));
+                Console.WriteLine($"Sending message #{i} {dataBuffer}");
+                dc.SendEventAsync(eventMessage).Wait();
+                //Console.WriteLine($"Message# {i} sent.");
+                i++;
+                Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+            }
         }
 
         private static void Advertise()
@@ -170,5 +232,17 @@ namespace poc
             return GetIPAddresses()
                 .Where(a => a.AddressFamily == AddressFamily.InterNetwork);
         }
+    }
+
+    public class MessageBody
+    {
+        [JsonProperty(PropertyName = "machine")]
+        public string Message { get; set; }
+
+        [JsonProperty(PropertyName = "ambient")]
+        public double Temperature { get; set; }
+
+        [JsonProperty(PropertyName = "timeCreated")]
+        public DateTime TimeCreated { get; set; }
     }
 }
